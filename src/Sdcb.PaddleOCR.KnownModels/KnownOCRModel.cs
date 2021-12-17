e8a082340db06cb9d2289a3886c9085003a832d7
sdcb <sdflysha@qq.com>
2021-12-17 15:37:38 +0800

    Add knownmodels project.

---
+++ src/Sdcb.PaddleOCR.KnownModels/KnownOCRModel.cs
@@ -0,0 +1,40 @@
+﻿using SharpCompress.Archives;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sdcb.PaddleOCR.KnownModels
+{
+    public class KnownOCRModel
+    {
+        private static readonly Uri ChineseKeyUri = new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/ppocr_keys_v1.txt");
+        private static readonly Uri EnglishKeyUri = new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/en_dict.txt");
+
+        public static OCRModel PPOcrV2 = new OCRModel("ppocr-v2",
+            new Uri(@"https://paddleocr.bj.bcebos.com/PP-OCRv2/chinese/ch_PP-OCRv2_det_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/PP-OCRv2/chinese/ch_PP-OCRv2_rec_infer.tar"),
+            ChineseKeyUri);
+
+        public static OCRModel PPOcrMobileV2 = new OCRModel("ppocr-mobile-v2",
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar"),
+            ChineseKeyUri);
+
+        public static OCRModel EnPPOcrMobileV2 = new OCRModel("en-ppocr-mobile-v2",
+            PPOcrV2.DetectionModelUri,
+            PPOcrV2.ClassifierModelUri,
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/multilingual/en_number_mobile_v2.0_rec_infer.tar"),
+            EnglishKeyUri);
+
+        public static OCRModel PPOcrServerV2 = new OCRModel("ppocr-server-v2",
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_det_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"),
+            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_rec_infer.tar"),
+            ChineseKeyUri);
+    }
+}
---
+++ src/Sdcb.PaddleOCR.KnownModels/OCRModel.cs
@@ -0,0 +1,97 @@
+﻿using SharpCompress.Archives;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sdcb.PaddleOCR.KnownModels
+{
+    public partial class OCRModel
+    {
+        public OCRModel(string name, Uri detectionModelUri, Uri classifierModelUri, Uri recognitionModelUri, Uri keyUri)
+        {
+            Name = name;
+            DetectionModelUri = detectionModelUri;
+            ClassifierModelUri = classifierModelUri;
+            RecognitionModelUri = recognitionModelUri;
+            KeyUri = keyUri;
+        }
+
+        public Uri DetectionModelUri { get; }
+        public Uri ClassifierModelUri { get; }
+        public Uri RecognitionModelUri { get; }
+        public Uri KeyUri { get; }
+        public string Name { get; }
+
+        public string RootDirectory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Name);
+
+        private async Task EnsureModelFile(Uri uri, string prefix, CancellationToken cancellationToken = default)
+        {
+            string directory = Path.Combine(RootDirectory, prefix);
+            string paramsFile = Path.Combine(directory, "inference.pdiparams");
+            Directory.CreateDirectory(directory);
+
+            if (!File.Exists(paramsFile))
+            {
+                string localTarFile = Path.Combine(directory, uri.Segments.Last());
+                if (!File.Exists(localTarFile))
+                {
+                    Console.WriteLine($"Downloading {prefix} model from {uri}");
+                    await DownloadFile(uri, localTarFile, cancellationToken);
+                }
+
+                Console.WriteLine($"Extracting {localTarFile} to {directory}");
+                using (IArchive archive = ArchiveFactory.Open(localTarFile))
+                {
+                    archive.WriteToDirectory(directory);
+                }
+
+                File.Delete(localTarFile);
+            }
+        }
+
+        private static async Task DownloadFile(Uri uri, string localFile, CancellationToken cancellationToken)
+        {
+            using HttpClient http = new();
+
+            HttpResponseMessage resp = await http.GetAsync(uri, cancellationToken);
+            if (!resp.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to download: {uri}, status code: {(int)resp.StatusCode}({resp.StatusCode})");
+            }
+
+            using (FileStream file = File.OpenWrite(localFile))
+            {
+                await resp.Content.CopyToAsync(file/*, cancellationToken*/);
+            }
+        }
+
+        public string DetectionDirectory => Path.Combine(RootDirectory, "det");
+        public string ClassifierDirectory => Path.Combine(RootDirectory, "cls");
+        public string RecognitionDirectory => Path.Combine(RootDirectory, "rec");
+        public string KeyPath => Path.Combine(RootDirectory, "key.txt");
+
+        public Task EnsureDetectionModel(CancellationToken cancellationToken = default) => EnsureModelFile(DetectionModelUri, "det", cancellationToken);
+        public Task EnsureClassifierModel(CancellationToken cancellationToken = default) => EnsureModelFile(ClassifierModelUri, "cls", cancellationToken);
+        public Task EnsureRecognitionModel(CancellationToken cancellationToken = default) => EnsureModelFile(RecognitionModelUri, "cls", cancellationToken);
+        public async Task EnsureKeyFile(CancellationToken cancellationToken = default)
+        {
+            if (!File.Exists(KeyPath))
+            {
+                Console.WriteLine($"Downloading key file {KeyPath} from {KeyUri}");
+                await DownloadFile(KeyUri, KeyPath, cancellationToken);
+            }
+        }
+
+        public Task EnsureAll(CancellationToken cancellationToken = default)
+        {
+            return Task.WhenAll(
+                EnsureDetectionModel(cancellationToken),
+                EnsureClassifierModel(cancellationToken),
+                EnsureRecognitionModel(cancellationToken),
+                EnsureKeyFile(cancellationToken));
+        }
+    }
+}
