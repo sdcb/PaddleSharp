b88795eecf0733a5aa13aafb3e66a8452276aa97
sdcb <sdflysha@qq.com>
2021-12-28 10:59:17 +0800

    Add ChineseTranditional model, move ChineseKeyUris order.

--- src/Sdcb.PaddleOCR.KnownModels/KnownOCRModel.cs
+++ src/Sdcb.PaddleOCR.KnownModels/KnownOCRModel.cs
@@ -6,13 +6,18 @@ namespace Sdcb.PaddleOCR.KnownModels
     {
         private static readonly Uri[] ChineseKeyUris = new[]
         {
-            new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/ppocr_keys_v1.txt"), /* Github */
             new Uri(@"https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.3/ppocr/utils/ppocr_keys_v1.txt"), /* Gitee */
+            new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/ppocr_keys_v1.txt"), /* Github */
         };
         private static readonly Uri[] EnglishKeyUris = new[]
         {
             new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/en_dict.txt"), /* Github */
             new Uri(@"https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.3/ppocr/utils/en_dict.txt"), /* Gitee */
+        };
+        private static readonly Uri[] ChineseTranditionalKeyUris = new[]
+        {
+            new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.4/ppocr/utils/dict/chinese_cht_dict.txt"), /* Github */
+            new Uri(@"https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.4/ppocr/utils/dict/chinese_cht_dict.txt"), /* Gitee */
         };
 
         public static OCRModel PPOcrV2 = new OCRModel("ppocr-v2",
@@ -28,11 +33,17 @@ namespace Sdcb.PaddleOCR.KnownModels
         //    new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar"),
         //    ChineseKeyUris);
 
-        public static OCRModel EnPPOcrMobileV2 = new OCRModel("en-ppocr-mobile-v2",
+        public static OCRModel EnglishMobileV2 = new OCRModel("en-ppocr-mobile-v2",
             PPOcrV2.DetectionModelUris,
             PPOcrV2.ClassifierModelUris,
             new[] { new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/multilingual/en_number_mobile_v2.0_rec_infer.tar") },
             EnglishKeyUris);
+
+        public static OCRModel ChineseTranditionalMobileV2 = new OCRModel("chinese-cht-mobile-v2",
+            PPOcrV2.DetectionModelUris,
+            PPOcrV2.ClassifierModelUris,
+            new[] { new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/multilingual/chinese_cht_mobile_v2.0_rec_infer.tar") },
+            ChineseTranditionalKeyUris);
 
         public static OCRModel PPOcrServerV2 = new OCRModel("ppocr-server-v2",
             new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_server_v2.0_det_infer.tar"),
