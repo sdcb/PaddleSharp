97a4d983ec159f0e6e3c69b47cd4bbf84d2666c5
sdcb <sdflysha@qq.com>
2022-06-27 14:47:43 +0800

    Upgrade docker profile to 2.3.0

--- build/docker/build-sdk.bat
+++ build/docker/build-sdk.bat
@@ -1,6 +1,6 @@
-cd dotnet6sdk-focal-paddle2.2.2
-docker build . -t sdflysha/dotnet6sdk-focal-paddle2.2.2
-docker push sdflysha/dotnet6sdk-focal-paddle2.2.2
+cd dotnet6sdk-focal-paddle
+docker build . -t sdflysha/dotnet6sdk-focal-paddle:2.3.0
+docker push sdflysha/dotnet6sdk-focal-paddle:2.3.0
 cd ..
 
 pause
