9e0f8062df9bef43dde2772aff87c9a39c29358a
sdcb <sdflysha@qq.com>
2022-06-27 19:03:00 +0800

    Fix the wrong en-ppocr-v3 dict.

--- src/Sdcb.PaddleOCR.KnownModels/KnownOCRModel.cs
+++ src/Sdcb.PaddleOCR.KnownModels/KnownOCRModel.cs
@@ -7,7 +7,6 @@ namespace Sdcb.PaddleOCR.KnownModels
         private static readonly Uri[] ChineseKeyUris = new[]
         {
             new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/ppocr_keys_v1.txt"), /* Github */
-            new Uri(@"https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.3/ppocr/utils/ppocr_keys_v1.txt"), /* Gitee */
         };
 
         public static OCRModel PPOcrV2 = new OCRModel("ppocr-v2",
@@ -16,13 +15,6 @@ namespace Sdcb.PaddleOCR.KnownModels
             new Uri(@"https://paddleocr.bj.bcebos.com/PP-OCRv2/chinese/ch_PP-OCRv2_rec_infer.tar"),
             ChineseKeyUris);
 
-        // this model not correct.
-        //public static OCRModel PPOcrMobileV2 = new OCRModel("ppocr-mobile-v2",
-        //    new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_infer.tar"),
-        //    new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"),
-        //    new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar"),
-        //    ChineseKeyUris);
-
         public static OCRModel EnglishMobileV2 = new OCRModel("en-ppocr-mobile-v2",
             PPOcrV2.DetectionModelUris,
             PPOcrV2.ClassifierModelUris,
@@ -30,7 +22,6 @@ namespace Sdcb.PaddleOCR.KnownModels
             new[]
             {
                 new Uri(@"https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/en_dict.txt"), /* Github */
-                new Uri(@"https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.3/ppocr/utils/en_dict.txt"), /* Gitee */
             });
 
         public static OCRModel PPOcrServerV2 = new OCRModel("ppocr-server-v2",
@@ -180,6 +171,7 @@ namespace Sdcb.PaddleOCR.KnownModels
            new Uri(@"https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_det_infer.tar"),
            new Uri(@"https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"),
            new Uri(@"https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_rec_infer.tar"),
-           ChineseKeyUris);
+           new[] { new Uri("https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.3/ppocr/utils/en_dict.txt") }
+           );
     }
 }
