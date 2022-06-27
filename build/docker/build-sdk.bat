53c90fff101b6fda1917e741dffaeadb0129077c
sdcb <sdflysha@qq.com>
2022-06-27 14:48:56 +0800

    Add docker build profile.

--- build/docker/build-sdk.bat
+++
@@ -1,6 +0,0 @@
-cd dotnet6sdk-focal-paddle
-docker build . -t sdflysha/dotnet6sdk-focal-paddle:2.3.0
-docker push sdflysha/dotnet6sdk-focal-paddle:2.3.0
-cd ..
-
-pause
--- build/docker/build.bat
+++
@@ -1,6 +0,0 @@
-cd dotnet6-focal-paddle
-docker build . -t sdflysha/dotnet6-focal-paddle:2.3.0
-docker push sdflysha/dotnet6-focal-paddle:2.3.0
-cd ..
-
-pause
