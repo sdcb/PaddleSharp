6309b59f9cb5255193438a65d40799a8534d4db2
n0099 <n@n0099.net>
2023-03-04 22:42:43 +0800

    add more combinations of `det+cls+rec` models for all available languages @ `Sdcb.PaddleOCR.Models.Online.OnlineFullModels`

--- src/Sdcb.PaddleOCR.Models.Online/OnlineFullModels.cs
+++ src/Sdcb.PaddleOCR.Models.Online/OnlineFullModels.cs
@@ -1,4 +1,4 @@
-﻿using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR.Models;
 using Sdcb.PaddleOCR.Models.Details;
 using System;
 using System.Collections.Generic;
@@ -28,8 +28,26 @@ namespace Sdcb.PaddleOCR.Models.Online
 
         public readonly static OnlineFullModels EnglishV3 = new(OnlineDetectionModel.EnglishV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.EnglishV3);
 
-        public readonly static OnlineFullModels TranditionalChinseV3 = new(OnlineDetectionModel.ChineseV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.TranditionalChineseV3);
+        public readonly static OnlineFullModels EnglishV3Slim = new(OnlineDetectionModel.EnglishV3Slim, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.EnglishV3Slim);
 
-        // free to make any other combinations
+        public readonly static OnlineFullModels KoreanV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.KoreanV3);
+
+        public readonly static OnlineFullModels JapanV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.JapanV3);
+
+        public readonly static OnlineFullModels TranditionalChineseV3 = new(OnlineDetectionModel.ChineseV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.TranditionalChineseV3);
+
+        public readonly static OnlineFullModels TeluguV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.TeluguV3);
+
+        public readonly static OnlineFullModels KannadaV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.KannadaV3);
+
+        public readonly static OnlineFullModels TamilV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.TamilV3);
+
+        public readonly static OnlineFullModels LatinV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.LatinV3);
+
+        public readonly static OnlineFullModels ArabicV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.ArabicV3);
+
+        public readonly static OnlineFullModels CyrillicV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.CyrillicV3);
+
+        public readonly static OnlineFullModels DevanagariV3 = new(OnlineDetectionModel.MultiLanguageV3, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.DevanagariV3);
     }
 }
