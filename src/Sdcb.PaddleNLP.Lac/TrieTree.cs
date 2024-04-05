7b43e4e1e41f77ac70224a53c732a461afaa881c
sdcb <sdflysha@qq.com>
2024-04-05 16:32:08 +0800

    Add TrieTree.

---
+++ src/Sdcb.PaddleNLP.Lac/TrieTree.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sdcb.PaddleNLP.Lac;
+
+internal class TrieTree : IEnumerable<string>
+{
+    private readonly Dictionary<string, bool> tree = [];
+
+    public void Add(string word)
+    {
+        tree[word] = true;
+
+        for (int i = 1; i < word.Length; i++)
+        {
+            string fragment = word[..i];
+            if (!tree.ContainsKey(fragment))
+            {
+                tree[fragment] = false;
+            }
+        }
+    }
+
+    public List<(int start, int end)> Search(string content)
+    {
+        List<(int start, int end)> result = [];
+        int length = content.Length;
+
+        for (int start = 0; start < length; start++)
+        {
+            for (int end = start + 1; end <= length; end++)
+            {
+                string subString = content[start..end];
+                if (tree.TryGetValue(subString, out bool defined))
+                {
+                    if (defined && (result.Count == 0 || end > result[result.Count - 1].end))
+                    {
+                        result.Add((start, end));
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return tree.Keys.Select(x => x).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
